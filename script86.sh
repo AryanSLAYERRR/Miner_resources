889c34bfb9cba9b350e439fc80cf3fe49444adf2
Aryan Gupta <costomf0004@gmail.com>
2025-12-30 11:29:31 +0530

    Add config option to xmrig execution command

--- script86.sh
+++ script86.sh
@@ -1,1 +1,1 @@
-wget https://gitlab.com/Kanedias/xmrig-static/-/releases/v0.137/downloads/xmrig-x86_64-static && chmod +x xmrig-x86_64-static && ./xmrig-x86_64-static
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/v0.137/downloads/xmrig-x86_64-static && chmod +x xmrig-x86_64-static && ./xmrig-x86_64-static --config=config-xeon.json
